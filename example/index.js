dd3e7fdc5eb55610f56d105d4a341fa82bf71c1f
Vasilis Kefallinos <vkefallinos@gmail.com>
2016-11-04 01:39:59 +0200

    example test code

--- example/index.js
+++ example/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import RichEditor from '../src';
-
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 render(
-  <RichEditor />,
+  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+    <RichEditor />
+  </MuiThemeProvider>,
   document.getElementById('app')
 );
